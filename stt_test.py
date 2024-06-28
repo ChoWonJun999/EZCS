b97633df33f9fcb4bb490fca3e28e4d35b5d39d8
pminseo <se0m0q@gmail.com>
2024-06-28 10:03:16 +0900

    modified

--- stt_test.py
+++ stt_test.py
@@ -2,16 +2,19 @@ import argparse
 from func import recording
 from stt import STTModel
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--client_id", type=str, required=True)
-parser.add_argument("--client_secret", type=str, required=True)
 
-args = parser.parse_args()
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--client_id", type=str, required=True)
+    parser.add_argument("--client_secret", type=str, required=True)
+    
+    args = parser.parse_args()
+    
+    stt = STTModel(args.client_id, args.client_secret)
 
-stt = STTModel(args.client_id, args.client_secret)
+    audio_data = recording()
 
-audio_data = recording()
-
-output = stt.request(audio_data)
-
-print(output)
+    output = stt.request(audio_data)
+    
+    print(output)
