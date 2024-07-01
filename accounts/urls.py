65dc09fb4305e056fc1214e51bcb83d96ae6e6c2
jhkim <wnsgnl9179@naver.com>
2024-07-01 17:30:08 +0900

    'login_admin'

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,9 +7,10 @@ from django.conf import settings
 app_name = 'accounts'
 
 urlpatterns = [
-
-    #path('signup/', views.signup, name='signup'),
     #path('login/', views.login_view, name='login'),
     path('', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('check_username/', views.check_username, name='check_username'),
+    path('check_email/', views.check_email, name='check_email'),
 ]
